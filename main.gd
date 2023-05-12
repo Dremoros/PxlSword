9af5f14120465bab8a7587c523d6d9f0a2199ae6
Dremoros <arthur.kluytmans@epita.fr>
2023-05-12 17:23:23 +0200

    Basic map done

--- main.gd
+++ main.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var Room = preload("res://room.tscn")
 
+@onready var Map = $TileMap
+
 #tile size of the room's tile in pxl
 var tile_size = 32
 
@@ -70,6 +72,8 @@ func _input(event):
 			n.queue_free()
 		path = null
 		make_rooms()
+	if event.is_action_pressed('ui_focus_next'):
+		make_map()
 	
 func find_mst(nodes):
 	# Prim's algorithm
@@ -97,3 +101,58 @@ func find_mst(nodes):
 	return path
 			
 	
+func make_map():
+	Map.clear()
+	var full_rect = Rect2()
+	for room in $Rooms.get_children():
+		var r = Rect2(room.position - room.size,
+					room.get_node("CollisionShape2D").shape.extents*2)
+		full_rect = full_rect.merge(r)
+	var topleft = Map.local_to_map(full_rect.position)
+	var bottomright = Map.local_to_map(full_rect.end)
+	for x in range(topleft.x, bottomright.x):
+		for y in range(topleft.y, bottomright.y):
+			Map.set_cell(0, Vector2i(x, y), 0, Vector2i(1,0), 0)
+	
+	# Carve the rooms from the walls
+	var corridors = [] # One corridor per connection
+	for room in $Rooms.get_children():
+		var s = (room.size / tile_size).floor()
+		var pos = Map.local_to_map(room.position)
+		var ul = (room.position / tile_size).floor() - s
+		#This iteration range create the space between the rooms
+		for x in range(2, s.x * 2 - 1):
+			for y in range(2, s.y * 2 - 1):
+				Map.set_cell(0, Vector2i(ul.x + x, ul.y + y), 0, Vector2i(0,0), 0)
+		# Carve connecting corridor
+		var p = path.get_closest_point(room.position)
+		for conn in path.get_point_connections(p):
+			if not conn in corridors:
+				var start = Map.local_to_map(Vector2(path.get_point_position(p).x, 
+													path.get_point_position(p).y))
+				var end = Map.local_to_map(Vector2(path.get_point_position(conn).x, 
+													path.get_point_position(conn).y))
+				carve_path(start, end)
+		corridors.append(p)
+
+func carve_path(start, end):
+	
+	var difference_x = sign(end.x - start.x)
+	var difference_y = sign(end.y - start.y)
+	
+	if difference_x == 0:
+		difference_x = pow(-1.0, randi() % 2)
+	if difference_y == 0:
+		difference_y = pow(-1.0, randi() % 2)
+		
+	var x_over_y = start
+	var y_over_x = end
+	
+	if randi() % 2 > 0:
+		x_over_y = end
+		y_over_x = start
+
+	for x in range(start.x, end.x, difference_x):
+		Map.set_cell(0, Vector2i(x, y_over_x.y), 0, Vector2i(0,0), 0)
+	for y in range(start.y, end.y, difference_y):
+		Map.set_cell(0, Vector2i(x_over_y.x, y), 0, Vector2i(0,0), 0)
